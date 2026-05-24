786d813e4764c468fd334df5455b3aa6d245ef34
iareb <lolluz6@gmail.com>
2026-05-24 17:33:58 +0200

    Added nlohmann/json library, created DialogueParser class

--- DialogueParser.h
+++ DialogueParser.h
@@ -1,1 +1,12 @@
 #pragma once
+#include "nlohmann/json.hpp"
+#include "DialogueGraph.h"
+
+using json = nlohmann::json;
+
+namespace Dialogue {
+	class DialogueParser {
+	private:
+		json JsonFile;
+	};
+}
